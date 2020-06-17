ff9db7b82be23b7dd6db6f249ac09d551af5e5ff
mmy83 <mmy83@126.com>
2020-06-17 21:34:23 +0800

    调整返回状态码

--- pkg/response/jsonresponse/response_msg.go
+++ pkg/response/jsonresponse/response_msg.go
@@ -16,29 +16,17 @@ var msg = make(map[int]Message)
 
 func init(){
 	msg[200000] = Message{ Code: 200000, Msg: "成功！" }
-	msg[200600] = Message{ Code: 200600, Msg: "成功！" }
-	msg[200601] = Message{ Code: 200601, Msg: "登录成功！" }
-	//msg[200602] = Message{ Code: 200602, Msg: "成功！" }
-	//msg[200603] = Message{ Code: 200603, Msg: "成功！" }
-	//msg[200604] = Message{ Code: 200604, Msg: "成功！" }
-	//msg[200605] = Message{ Code: 200605, Msg: "成功！" }
-	//msg[200606] = Message{ Code: 200606, Msg: "成功！" }
-	//msg[200607] = Message{ Code: 200607, Msg: "成功！" }
-	//msg[200608] = Message{ Code: 200608, Msg: "成功！" }
-	//msg[200609] = Message{ Code: 200609, Msg: "成功！" }
-	//msg[200610] = Message{ Code: 200610, Msg: "成功！" }
 
-	msg[200700] = Message{ Code: 200700, Msg: "失败！" }
-	msg[200701] = Message{ Code: 200701, Msg: "登录失败！" }
-	msg[200702] = Message{ Code: 200702, Msg: "用户不存在！" }
-	msg[200703] = Message{ Code: 200703, Msg: "验证码错误！" }
-	msg[200704] = Message{ Code: 200704, Msg: "密码错误！" }
-	msg[200705] = Message{ Code: 200705, Msg: "缺少参数！" }
-	msg[200706] = Message{ Code: 200706, Msg: "获取数据失败！" }
-	msg[200707] = Message{ Code: 200707, Msg: "没有权限！" }
-	msg[200708] = Message{ Code: 200708, Msg: "没有管理员权限！" }
-	msg[200709] = Message{ Code: 200709, Msg: "创建失败！" }
-
+	msg[200500] = Message{ Code: 200500, Msg: "失败！" }
+	msg[200501] = Message{ Code: 200501, Msg: "登录失败！" }
+	msg[200502] = Message{ Code: 200502, Msg: "用户不存在！" }
+	msg[200503] = Message{ Code: 200503, Msg: "验证码错误！" }
+	msg[200504] = Message{ Code: 200504, Msg: "密码错误！" }
+	msg[200505] = Message{ Code: 200505, Msg: "缺少参数！" }
+	msg[200506] = Message{ Code: 200506, Msg: "获取数据失败！" }
+	msg[200507] = Message{ Code: 200507, Msg: "没有权限！" }
+	msg[200508] = Message{ Code: 200508, Msg: "没有管理员权限！" }
+	msg[200509] = Message{ Code: 200509, Msg: "创建失败！" }
 
 }
 
